53ca23c2d2647f767868b5e4c5e24863365cdd7f
MARJORY DE SOUSA VILLAS BOAS <26171862@edu119.local>
2026-09-17 09:52:56 -0300

    RevisaoPrimeiraAPI

---
+++ servidor.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.get("/",(req,res) =>{
+    res.json({
+        mensagem: "API Alunos funcionando"
+    })
+})
+
+const PORTA = 3000;
+app.listen(PORTA, () =>{
+    console.log("Servisor iniciado com sucesso");
+    console.log(`http://localhost:${PORTA}`);
+});
